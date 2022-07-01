83a9e07d7f3e346e02efa6f835c712fed0595b8d
Artem Iefymenko <iefymenko.artem@gmail.com>
2022-07-01 14:29:59 -0400

    Exercise 7 completed

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,7 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+  validates :first_name, presence: {message: "First name is required"}
+  validates :last_name, presence: {message: "Last name is required"}
+  validates :hourly_rate, numericality: {only_integer: true, inclusion: 40..200}
+  validates :store, presence: true
 end
